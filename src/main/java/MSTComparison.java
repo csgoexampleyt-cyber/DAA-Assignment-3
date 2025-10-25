3d668a4af270ab43ee4c23887959ef826c413add
csgoexampleyt-cyber <csgoexampleyt@gmail.com>
2025-10-25 12:48:49 +0500

    UNION-FIND: UnionFind.

--- src/main/java/MSTComparison.java
+++ src/main/java/MSTComparison.java
@@ -102,3 +102,50 @@ class MSTResult {
         return json;
     }
 }
+
+//UNION-FIND
+
+class UnionFind {
+    int[] parent;
+    int[] rank;
+    int operations;
+
+    public UnionFind(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        operations = 0;
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    public int find(int x) {
+        operations++;
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    public boolean union(int x, int y) {
+        operations++;
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX == rootY) {
+            return false;
+        }
+
+        if (rank[rootX] < rank[rootY]) {
+            parent[rootX] = rootY;
+        } else if (rank[rootX] > rank[rootY]) {
+            parent[rootY] = rootX;
+        } else {
+            parent[rootY] = rootX;
+            rank[rootX]++;
+        }
+
+        return true;
+    }
+}
