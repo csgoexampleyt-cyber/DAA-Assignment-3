2596bcf7a587fe5fd115c6160659433ce87f7d55
csgoexampleyt-cyber <csgoexampleyt@gmail.com>
2025-10-25 12:30:43 +0500

    DATA STRUCTURES: MSTResult and toJson

--- src/main/java/MSTComparison.java
+++ src/main/java/MSTComparison.java
@@ -55,3 +55,50 @@ class Graph {
         return edges.size();
     }
 }
+
+class MSTResult {
+    String algorithm;
+    int vertices;
+    int originalEdges;
+    List<Edge> mstEdges;
+    double totalCost;
+    int operations;
+    double executionTimeMs;
+    boolean isConnected;
+
+    public MSTResult(String algorithm, int vertices, int originalEdges) {
+        this.algorithm = algorithm;
+        this.vertices = vertices;
+        this.originalEdges = originalEdges;
+        this.mstEdges = new ArrayList<>();
+        this.totalCost = 0.0;
+        this.operations = 0;
+        this.executionTimeMs = 0.0;
+        this.isConnected = true;
+    }
+
+    public JsonObject toJson() {
+        JsonObject json = new JsonObject();
+        json.addProperty("algorithm", algorithm);
+        json.addProperty("vertices", vertices);
+        json.addProperty("original_edges", originalEdges);
+
+        JsonArray edgesArray = new JsonArray();
+        for (Edge edge : mstEdges) {
+            JsonObject edgeObj = new JsonObject();
+            edgeObj.addProperty("from", edge.from);
+            edgeObj.addProperty("to", edge.to);
+            edgeObj.addProperty("weight", Math.round(edge.weight * 100.0) / 100.0);
+            edgesArray.add(edgeObj);
+        }
+        json.add("mst_edges", edgesArray);
+
+        json.addProperty("mst_edge_count", mstEdges.size());
+        json.addProperty("total_cost", Math.round(totalCost * 100.0) / 100.0);
+        json.addProperty("operations", operations);
+        json.addProperty("execution_time_ms", Math.round(executionTimeMs * 10000.0) / 10000.0);
+        json.addProperty("is_connected", isConnected);
+
+        return json;
+    }
+}
